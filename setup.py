fcd12110ac98738355d1eae5718e909b3691be60
lethblak <ma.leclercq@student.uclouvain.be>
2022-06-05 23:31:38 +0200

    more comment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="inginious-dispenser-cat",
-    version="0.1dev0",
+    version="1.0",
     description="Plugin to add cat task dispenser",
     packages=find_packages(),
     install_requires=["inginious>=0.8dev0"],
@@ -13,7 +13,7 @@ setup(
     extras_require={},
     scripts=[],
     include_package_data=True,
-    author="The INGInious authors",
+    author="Matthieu Leclercq et Clement Linsmeau",
     author_email="inginious@info.ucl.ac.be",
     license="AGPL 3",
     url="https://github.com/maleclercq/CAT-Dispenser-Plugin"
